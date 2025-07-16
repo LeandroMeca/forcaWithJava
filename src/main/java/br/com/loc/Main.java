6822f62ea7d17c2f9b048ab5b17ef83f8c11d91a
Leandro Cruz <LeandroCruz@hotmail.com>
2025-07-16 05:41:10 -0300

    creating menu of the game

--- src/main/java/br/com/loc/Main.java
+++ src/main/java/br/com/loc/Main.java
@@ -1,19 +1,48 @@
 package br.com.loc;
 
+import br.com.loc.hangman.exception.GameIsFinishedException;
 import br.com.loc.hangman.model.HangmanChar;
 import br.com.loc.hangman.model.HangmanGame;
 
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 
 public class Main {
 
+    private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String... args) {
 
         var characters = Stream.of(args).map(a -> a.toLowerCase().charAt(0)).map(HangmanChar::new).toList();
         System.out.println(characters);
-        System.out.println(new HangmanGame(characters));
+        var hangmanGame = new HangmanGame(characters);
+
+        System.out.println("Bem vindo ao jogo da forca, tente adivinhar a palavra, boa sorte");
+        System.out.println(hangmanGame);
+        System.out.println("Selecione uma das opções:");
+
+        var option = -1;
+        while (option != 3){
+            System.out.println("1 - Informar uma letra");
+            System.out.println("2 - Verificar status do jogo");
+            System.out.println("3 - Sair do jogo");
+            option = scanner.nextInt();
+            if((option > 3) || (option < 1)){
+                System.out.println("Opção inválida");
+            }
+            if(option == 1){
+                System.out.println("Informe uma letra");
+                var character = scanner.next().charAt(0);
+                try{
+                    hangmanGame.inputCharacter(character);
+                }catch (GameIsFinishedException ex){
+                    System.out.println(ex.getMessage());
+                    System.exit(0);
+                }
+            }
+        }
+
     }
 
 
